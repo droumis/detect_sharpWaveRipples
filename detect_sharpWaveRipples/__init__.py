d308b82d43f40414d0f9d1e729f274bd5f0ec826
droumis <roumis.d@gmail.com>
2020-12-01 11:29:09 +0100

    initial commit

---
+++ detect_sharpWaveRipples/__init__.py
@@ -0,0 +1,5 @@
+"""detect_sharpWaveRipples - detect sharp wave ripple events from electrophysiological data"""
+
+__version__ = '\0.1.0'
+__author__ = 'Demetris Roumis <roumis.d+package@gmail.com>'
+__all__ = []
